b915810c995aacdcb5f2d6769c59d24c0c8a54f6
Charles Onyango <charles.onyango@student.moringaschool.com>
2023-06-30 23:50:40 +0300

    complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def show
+        user = find_users
+        render json: user, except: [:created_at, :updated_at], include: {items: { except: [:created_at, :updated_at]}}
+    end
+
+    private
+
+    def find_users
+        User.find(params[:id])
+    end
+
+
 end
